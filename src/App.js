daf93c962eb8a6205605b5496325762581c32d78
AgtLucas <lucas@agtlucas.com>
2017-08-27 08:10:19 +0200

    Fix typos

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <Image accessibilityLabel='React Logo' resizeMode='contain' source={logo} style={styles.logo} alt='logo' />
           <Text style={styles.title}>Welcome to React</Text>
         </View>
-        <Text style={styles.intro}>
+        <Text style={styles.info}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </Text>
       </View>
@@ -19,10 +19,6 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  app: {
-    textAlign: 'center'
-  },
-
   logo: {
     height: 80
   },
